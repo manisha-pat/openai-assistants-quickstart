a9396b999520dcde6330ead49c2d1b39d12d1ef7
manisha-pat <manisha.patel@tatari.tv>
2024-12-10 13:42:19 -0800

    adding assistant id

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,6 @@
 export let assistantId = "asst_zvNAIFHITOa61z8gzbb466RU"; // set your assistant ID here
 
 if (assistantId === "") {
-  assistantId = process.env.OPENAI_ASSISTANT_ID;
+  assistantId = "asst_zvNAIFHITOa61z8gzbb466RU";
+  // process.env.OPENAI_ASSISTANT_ID;
 }
